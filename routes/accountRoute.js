9ba59ebedd923629b11e72457e32a7d15e361056
theassetportal <117767574+theassetportal@users.noreply.github.com>
2025-06-02 18:42:12 -0600

    controllers/accountController.js 	modified:   controllers/baseController.js 	modified:   controllers/invController.js 	new file:   database/assignment2.sql 	modified:   models/inventory-model.js 	modified:   package.json 	modified:   pnpm-lock.yaml 	modified:   routes/accountRoute.js 	modified:   routes/inventoryRoute.js 	modified:   server.js 	modified:   utilities/account-validation 	modified:   utilities/index.js 	new file:   utilities/inventory-validation 	new file:   views/account/register.ejs 	new file:   views/inventory/add-classification.ejs 	new file:   views/inventory/add-inventory.ejs 	new file:   views/inventory/edit-inventory.ejs 	new file:   views/inventory/management.ejs

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -7,13 +7,35 @@ const express = require("express")
 const router = new express.Router()
 const accountController = require("../controllers/accountController")
 const utilities = require("../utilities")
-//const regValidate = require("../utilities/account-validation")
+const regValidate = require("../utilities/account-validation")
 
 /* ************************************
  *  Deliver Login View
  *  Unit 4, deliver login view activity
  *  ******************************** */
 router.get("/login", utilities.handleErrors(accountController.buildLogin))
+
+/* ************************************
+ *  Deliver Registration View
+ *  Unit 4, deliver registration view activity
+ *  ******************************** */
+router.get("/register", utilities.handleErrors(accountController.buildRegister))
+
+
+router.post(
+  "/register",
+  regValidate.registationRules(),
+  regValidate.checkRegData,
+  utilities.handleErrors(accountController.registerAccount)
+)
+
+
+/* ************************************
+ *  Process Login
+ *  Unit 4, stickiness activity
+ *  Modified in Unit 5, Login Process activity
+ *  ******************************** */
+
 
 
 module.exports = router
--- server.js
+++ server.js
@@ -16,6 +16,48 @@ const accountRoute = require("./routes/accountRoute")
 const utilities = require("./utilities/")
 const session = require("express-session")
 const pool = require('./database/')
+const bodyParser = require("body-parser")
+
+
+/* ***********************
+ * Middleware
+ * Between the request and response
+ * ************************/
+// Unit 4, Sessions & Messages Activity
+app.use(
+  session({
+    store: new (require("connect-pg-simple")(session))({
+      createTableIfMissing: true,
+      pool,
+    }),
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    name: "sessionId",
+  })
+)
+// Unit 4, Sessions & Messages Activity
+// Express Messages Middleware
+app.use(require("connect-flash")())
+app.use(function (req, res, next) {
+  res.locals.messages = require("express-messages")(req, res)
+  next()
+})
+// Unit 4, Process Registration Activity
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+
+// Express Messages Middleware
+app.use(require('connect-flash')())
+app.use(function(req, res, next){
+  res.locals.messages = require('express-messages')(req, res)
+  next()
+})
+
+// Unit 4, Process Registration Activity
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 
 /* ***********************
@@ -26,40 +68,19 @@ app.use(expressLayouts)
 app.set("layout", "./layouts/layout") // not at views root
 
 
-/* ***********************
- * Middleware
- * ************************/
- app.use(session({
-  store: new (require('connect-pg-simple')(session))({
-    createTableIfMissing: true,
-    pool,
-  }),
-  secret: process.env.SESSION_SECRET,
-  resave: true,
-  saveUninitialized: true,
-  name: 'sessionId',
-}))
 
-// Express Messages Middleware
-app.use(require('connect-flash')())
-app.use(function(req, res, next){
-  res.locals.messages = require('express-messages')(req, res)
-  next()
-})
 
 
 /* ***********************
  * Routes
  *************************/
 app.use(static)
-//Index Route
+// Index route - Unit 3, Robust Error Handling activity
 app.get("/", utilities.handleErrors(baseController.buildHome))
-
-// Inventory routes
+// Inventory routes - Unit 3, Build Inventory route activity
 app.use("/inv", inventoryRoute)
-
-// Account routes
-app.use("/account", require("./routes/accountRoute"))
+// Account routes - Unit 4, Deliver Login activity
+app.use("/account", accountRoute)
 
 
 
